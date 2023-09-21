8379a8bd0ffe07a6cdb78748bd1e99fd3e932662
dmilleza <darrenmilleza@gmail.com>
2023-09-21 14:07:36 -0700

    added lessons
    added lessons 1 to 3

---
+++ 01/script.js
@@ -0,0 +1,11 @@
+// to execute a function or a method, you have to use parentheses
+console.log('hello');
+
+// dot notation syntax is used to access properties and methods
+
+// any piece of data in any programming language has a type
+
+// 95% of console methods you will use will be console.log
+
+const styles = 'padding: 10px; background-color: pink; color: aqua';
+console.log('%cHello everyone', styles);
